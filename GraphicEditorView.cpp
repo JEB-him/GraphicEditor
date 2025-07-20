2c84eb257d66be95c2adc5e99b16c68c33832213
Hao Jiang <241502015@smail.nju.edu.cn/>
2025-07-20 17:20:19 +0800

    Initialize the project files.

---
+++ GraphicEditorView.cpp
@@ -0,0 +1,128 @@
+﻿
+// GraphicEditorView.cpp: CGraphicEditorView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "GraphicEditor.h"
+#endif
+
+#include "GraphicEditorDoc.h"
+#include "GraphicEditorView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CGraphicEditorView
+
+IMPLEMENT_DYNCREATE(CGraphicEditorView, CView)
+
+BEGIN_MESSAGE_MAP(CGraphicEditorView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CGraphicEditorView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CGraphicEditorView 构造/析构
+
+CGraphicEditorView::CGraphicEditorView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CGraphicEditorView::~CGraphicEditorView()
+{
+}
+
+BOOL CGraphicEditorView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CGraphicEditorView 绘图
+
+void CGraphicEditorView::OnDraw(CDC* /*pDC*/)
+{
+	CGraphicEditorDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CGraphicEditorView 打印
+
+
+void CGraphicEditorView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CGraphicEditorView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CGraphicEditorView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CGraphicEditorView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CGraphicEditorView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CGraphicEditorView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CGraphicEditorView 诊断
+
+#ifdef _DEBUG
+void CGraphicEditorView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CGraphicEditorView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CGraphicEditorDoc* CGraphicEditorView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CGraphicEditorDoc)));
+	return (CGraphicEditorDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CGraphicEditorView 消息处理程序
---
+++ GraphicEditorView.h
@@ -0,0 +1,52 @@
+﻿
+// GraphicEditorView.h: CGraphicEditorView 类的接口
+//
+
+#pragma once
+
+
+class CGraphicEditorView : public CView
+{
+protected: // 仅从序列化创建
+	CGraphicEditorView() noexcept;
+	DECLARE_DYNCREATE(CGraphicEditorView)
+
+// 特性
+public:
+	CGraphicEditorDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CGraphicEditorView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // GraphicEditorView.cpp 中的调试版本
+inline CGraphicEditorDoc* CGraphicEditorView::GetDocument() const
+   { return reinterpret_cast<CGraphicEditorDoc*>(m_pDocument); }
+#endif
+
